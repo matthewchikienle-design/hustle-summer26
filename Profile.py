a9929c4694c693d34f457f4747b66dd135c73410
Matthew Le <matthew@Matthews-MacBook-Pro.local>
2026-06-17 13:51:43 -0700

    Add Python profile variables lab

---
+++ Profile.py
@@ -0,0 +1,18 @@
+
+full_name = "Matthew Le"
+print(full_name)
+
+nickname = "Matt"
+print(nickname)
+age = 16
+print(age)
+used_python_before = True
+print(used_python_before)
+hobbies = ["gaming","track and field", "gym"]
+print(hobbies)
+favorites = {
+  "movie": "Avengars End Game",
+  "food":"garlic bread",
+  "hobby":"working out"
+}
+print(favorites)
